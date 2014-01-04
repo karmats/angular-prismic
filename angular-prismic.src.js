be607a950a22a5b0c419c99604c6c7fb5fba91c6
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-01-04 10:36:52 +0100

    More scaffolding

--- angular-prismic.src.js
+++ angular-prismic.src.js
@@ -1,30 +1,37 @@
-angular.module('prismic.io', []).provider('Prismic', function(){
+angular.module('prismic.io', [])
+    .value('PrismicBackend', function () {
+        return window.Prismic;
+    })
+    .provider('Prismic', function(PrismicBackend){
 
-    //'https://lesbonneschoses.prismic.io/api'
-    var _APIEndpoint = '';
+        //'https://lesbonneschoses.prismic.io/api'
+        var _APIEndpoint = '';
 
-    this.setAPIEndpoint = function (endpoint) {
-        _APIEndpoint = endpoint;
-    };
-    
-    this.$get = function () {
+        this.setAPIEndpoint = function (endpoint) {
+            _APIEndpoint = endpoint;
+        };
 
-        var _setAccessToken = function setAccessToken(token) {
+        this.$get = function () {
 
-            },
+            var _accesToken = '',
+                _setAccessToken = function setAccessToken(token) {
+                    _accesToken = token;
+                },
 
-            _setClientId = function setClientId(clientId) {
+                _clientId = '',
+                _setClientId = function setClientId(clientId) {
+                    _clientId = clientId;
+                },
 
-            },
+                _clientSecret = '',
+                _setClientSecret = function setClientSecret(clientSecret) {
+                    _clientSecret = clientSecret;
+                };
 
-            _setClientSecret = function setClientSecret(clientSecret) {
-
-            };
-
-        return {
-            setAccessToken: _setAccessToken,
-            setClientId: _setClientId,
-            setClientSecret: _setClientSecret
+            return {
+                setAccessToken: _setAccessToken,
+                setClientId: _setClientId,
+                setClientSecret: _setClientSecret
+            }
         }
-    }
-});
+    });
--- angular-prismic.test.js
+++ angular-prismic.test.js
@@ -1,69 +1,104 @@
 'use strict';
 
-describe('PrismicBackEnd', function () {
-   it('should proxy window.Prismic', function() {
-       
-   }); 
+describe('PrismicBackend', function () {
+
+    var PrismicBackend, $window;
+
+    beforeEach(function () {
+        module('prismic.io', function (_PrismicBackend_) {
+            PrismicBackend = _PrismicBackend_;
+        });
+
+        inject(function (_$window_) {
+            $window = _$window_;
+        });
+    });
+
+    it('should proxy window.Prismic', function () {
+        expect(PrismicBackend).toBe($window.Prismic);
+    });
 });
 
 describe('Provider configuration: prismicProvider configuration', function () {
     var prismicProvider, prismic;
 
-    beforeEach(function(){
+    beforeEach(function () {
         module('prismic.io', function (_prismicProvider_) {
             prismicProvider = _prismicProvider_;
         });
     });
 
-    it('should have a configuration API', function() {
+    it('should have a configuration API', function () {
         expect(typeof prismicProvider.setAPIEndpoint).toBe('function');
     });
 });
 
-describe('Provider: prismicProvider', function(){
+describe('Provider: prismicProvider', function () {
 
-    var $rootScope, Prismic, $httpBackend;
+    var $rootScope, Prismic, $httpBackend, PrismicBackend;
 
-    beforeEach(function(){
-        module('prismic.io', function (prismicProvider) {
+    beforeEach(function () {
+
+        PrismicBackend = jasmine.createSpy('PrismicBackend');
+
+        module('prismic.io', function ($provide) {
+            $provide.value('PrismicBackend', PrismicBackend);
         });
 
-        inject(function(_$rootScope_, _Prismic_, _$httpBackend_){
+        inject(function (_$rootScope_, _Prismic_, _$httpBackend_) {
             $rootScope = _$rootScope_;
             Prismic = _Prismic_;
             $httpBackend = _$httpBackend_;
         });
     });
 
-    it('should return something', function(){
-       expect(Prismic).toBeDefined();
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should return something', function () {
+        expect(Prismic).toBeDefined();
+    })
+    
+    it('should provide a custom resuest handler to the PismicBackend', function() {
+        //https://github.com/prismicio/javascript-kit/blob/master/src/api.js#L192
     });
 
     describe('setAccessToken', function () {
-        it('should have a setAccessToken function', function() {
+        it('should have a setAccessToken function', function () {
             expect(typeof Prismic.setAccessToken).toBe('function');
         });
 
-        it('should send the access token on requests to the API', function() {
-            expect(true).toBeFalsy();
+        it('should send the access token on requests to the API', function () {
+            //TODO: Implement
+            $httpBackend.expectGET('/auth').respond(201, '');
+            Prismic.setAccessToken('');
+            $httpBackend.flush();
         });
     });
 
     describe('OAuth', function () {
-        it('should have a setCliendId function', function() {
+        it('should have a setCliendId function', function () {
             expect(typeof Prismic.setCliendId).toBe('function');
         });
 
-        it('should send the client id on requests to the API', function() {
-            expect(true).toBeFalsy();
+        it('should send the client id on requests to the API', function () {
+            //TODO: Implement
+            $httpBackend.expectGET('/auth').respond(201, '');
+            Prismic.setCliendId('');
+            $httpBackend.flush();
         });
 
-        it('should have a setClientSecret function', function() {
+        it('should have a setClientSecret function', function () {
             expect(typeof Prismic.setClientSecret).toBe('function');
         });
 
-        it('should send the client secret on requests to the API', function() {
-            expect(true).toBeFalsy();
+        it('should send the client secret on requests to the API', function () {
+            //TODO: Implement
+            $httpBackend.expectGET('/auth').respond(201, '');
+            Prismic.setClientSecret('');
+            $httpBackend.flush();
         });
     })
 });
