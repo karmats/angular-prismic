7db1f835359dd82ebe5dfaa79777947a70bf7d16
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-01-06 19:58:35 +0100

    wip

--- angular-prismic.src.js
+++ angular-prismic.src.js
@@ -4,58 +4,6 @@ angular.module('prismic.io', [])
     })
     .provider('Prismic', function(PrismicBackend){
 
-        var parseQS = function(query) {
-            var params = {},
-                match,
-                pl = /\+/g,
-                search = /([^&=]+)=?([^&]*)/g,
-                decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); };
-            while (match = search.exec(query)) {
-                params[decode(match[1])] = decode(match[2]);
-            }
-            return params;
-        };
-
-        var getApiHome = function(callback) {
-            Prismic.Api(Configuration.apiEndpoint, callback, sessionStorage.getItem('ACCESS_TOKEN'));
-        };
-
-        var buildContext = function(ref, callback) {
-            // retrieve the API
-            getApiHome(function(api) {
-                var ctx = {
-                    ref: (ref || api.data.master.ref),
-                    api: api,
-                    maybeRef: (ref && ref != api.data.master.ref ? ref : ''),
-                    maybeRefParam: (ref && ref != api.data.master.ref ? '&ref=' + ref : ''),
-
-                    oauth: function() {
-                        var token = sessionStorage.getItem('ACCESS_TOKEN');
-                        return {
-                            accessToken: token,
-                            hasPrivilegedAccess: !!token
-                        }
-                    },
-
-                    linkResolver: function(doc) {
-                        return Configuration.linkResolver(ctx, doc);
-                    }
-                }
-                callback(ctx);
-            });
-        };
-
-        var withPrismic = function(callback) {
-            buildContext(queryString['ref'], function(ctx) {
-                callback.apply(window, [ctx]);
-            });
-        };
-
-        var queryString = parseQS(window.location.search.substring(1));
-
-        // Hash data
-        var encodedHash = parseQS(window.location.hash.substring(1));
-
         this.$get = function () {
 
             var _accessToken = '',
@@ -78,11 +26,73 @@ angular.module('prismic.io', [])
                     _APIEndpoint = endpoint;
                 };
 
+
+            ///////////
+
+
+
+            var parseQS = function(query) {
+                var params = {},
+                    match,
+                    pl = /\+/g,
+                    search = /([^&=]+)=?([^&]*)/g,
+                    decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); };
+                while (match = search.exec(query)) {
+                    params[decode(match[1])] = decode(match[2]);
+                }
+                return params;
+            };
+
+            var getApiHome = function(callback) {
+                PrismicBackend.Api(_APIEndpoint, callback, _accessToken);
+            };
+
+            var buildContext = function(ref, callback) {
+                // retrieve the API
+                getApiHome(function(api) {
+                    var ctx = {
+                        ref: (ref || api.data.master.ref),
+                        api: api,
+                        maybeRef: (ref && ref != api.data.master.ref ? ref : ''),
+                        maybeRefParam: (ref && ref != api.data.master.ref ? '&ref=' + ref : ''),
+
+                        oauth: function() {
+                            var token = _accessToken;
+                            return {
+                                accessToken: token,
+                                hasPrivilegedAccess: !!token
+                            }
+                        },
+
+                        linkResolver: function(doc) {
+                            return 'detail.html?id=' + doc.id + '&slug=' + doc.slug + ctx.maybeRefParam;
+                        }
+                    }
+                    callback(ctx);
+                });
+            };
+
+            var withPrismic = function(callback) {
+                buildContext(queryString['ref'], function(ctx) {
+                    callback.apply(window, [ctx]);
+                });
+            };
+
+            var queryString = parseQS(window.location.search.substring(1));
+
+            // Hash data
+            var encodedHash = parseQS(window.location.hash.substring(1));
+
+
+            //////////////
+
+
             return {
                 setAccessToken: _setAccessToken,
                 setClientId: _setClientId,
                 setClientSecret: _setClientSecret,
-                setAPIEndpoint: _setAPIEndpoint
+                setAPIEndpoint: _setAPIEndpoint,
+                get: withPrismic
             }
         }
     });
